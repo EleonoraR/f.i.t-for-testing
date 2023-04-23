8661aac55aca22263dd4e487b62a244fc9f61702
Ella R <61467087+EleonoraR@users.noreply.github.com>
2023-04-23 13:49:53 -0400

    alert was deleted buttonEventListeren was added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-alert("works");
+let helloMessage = "Hello beautiful female in tech";
+let btn = document.getElementById("click-btn");
+function pressBtn(){
+    alert(helloMessage);
+}
+ btn.addEventListener("click", pressBtn);
